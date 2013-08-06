e1eef34326e31dde09af19dce0f971f2b727c4ff
KhunAke <khun.ake@hotmail.com>
2013-08-06 14:44:59 +0700

    Class utility AES is success

--- src/com/javath/Configuration.java
+++ src/com/javath/Configuration.java
@@ -3,15 +3,31 @@ package com.javath;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Properties;
+import java.util.Random;
 import java.util.prefs.Preferences;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
+import org.apache.commons.codec.binary.Base64;
+
+import com.javath.util.AES;
 
 public class Configuration {
 	
-	private static Properties properties = new Properties();
-	private static Preferences preferences;
-	private static String application;
+	private static final Properties properties = new Properties();
+	private static final Preferences preferences;
+	private static final String application;
+	private static final AES aes;
 	
 	static {
 		try {
@@ -34,6 +50,14 @@ public class Configuration {
 	        else
 	        	preferences = Preferences.userRoot().node(application);
 	        
+	        String shared_key = preferences.get("SharedKey", "");
+	        if (shared_key.equals("")) {
+	        	shared_key = randomMD5();
+	        	preferences.put("SharedKey", shared_key);
+	        }
+	        
+	        aes = new AES(md5(shared_key));
+	        
 		} catch (FileNotFoundException e) { 
 			//logger.severe(message(e));
 			throw new ObjectException(e);
@@ -42,6 +66,32 @@ public class Configuration {
 			throw new ObjectException(e);
 		}
     }
+	
+	public static String md5(String message) {
+		try {
+			byte[] bytesOfMessage = message.getBytes("UTF-8");
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			byte[] digest = md.digest(bytesOfMessage);
+			return Base64.encodeBase64String(digest);
+		} catch (UnsupportedEncodingException e) {
+			//logger.severe(message(e));
+			throw new ObjectException(e);
+		} catch (NoSuchAlgorithmException e) {
+			//logger.severe(message(e));
+			throw new ObjectException(e);
+		}
+	}
+	
+	public static String randomMD5() {
+		char[] chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".toCharArray();
+		StringBuilder sb = new StringBuilder();
+		Random random = new Random();
+		for (int i = 0; i < 22; i++) {
+		    char c = chars[random.nextInt(chars.length)];
+		    sb.append(c);
+		}
+		return sb.toString() + "==";
+	}
 	
 	public static String getProperty(String key, String defaultValue) {
 		return properties.getProperty(key, defaultValue);
@@ -53,6 +103,59 @@ public class Configuration {
 	
 	public static Preferences getPreferenceNode(String node) {
 		return preferences.node(node);
+	}
+	
+	public static String encrypt(String message) {
+		return aes.encrypt(message);
+	}
+	
+	public static String decrypt(String message) {
+		return aes.decrypt(message);
+	}
+	
+	public static String getApplicationName() {
+		return application;
+	}
+	
+	public static void main(String[] args) {
+		Options options = initOptions();
+		if (args.length == 0) {
+			usage("Configuration",options);
+			return;
+		}
+		//CommandLineParser parser = new GnuParser();
+		CommandLineParser parser = new PosixParser();
+		try {
+			CommandLine cmd = parser.parse( options, args);
+			if (cmd.hasOption("encrypt")) {
+				String message = cmd.getOptionValue("encrypt");
+				System.out.println(Configuration.encrypt(message));
+			}
+		} catch (ParseException e) {
+			System.err.println(e.getMessage());
+			usage("Configuration",options);
+		}
+	}
+	
+	@SuppressWarnings("static-access")
+	private static Options initOptions() {
+		// create Options object
+		Options options = new Options();
+		
+		// add option
+		options.addOption( OptionBuilder.withLongOpt( "encrypt" )
+                .withDescription( "use encryption message" )
+                .hasArg()
+                .isRequired()
+                .withArgName("message")
+                .create("e") );
+		
+		return options;
+	}
+	
+	private static void usage(String command, Options options) {
+		HelpFormatter formatter = new HelpFormatter();
+		formatter.printHelp( command, options , true);
 	}
 	
 }
--- src/com/javath/util/AES.java
+++ src/com/javath/util/AES.java
@@ -1,103 +1,82 @@
 package com.javath.util;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
 
-public class AES {
-        /**
-         * Turns array of bytes into string
-         * 
-         * @param buf
-         *                      Array of bytes to convert to hex string
-         * @return Generated hex string
-         */
-        public static String asHex(byte buf[]) {
-                StringBuffer strbuf = new StringBuffer(buf.length * 2);
-                int i;
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.codec.binary.StringUtils;
 
-                for (i = 0; i < buf.length; i++) {
-                        if (((int) buf[i] & 0xff) < 0x10)
-                                strbuf.append("0");
+import com.javath.Configuration;
+import com.javath.Object;
+import com.javath.ObjectException;
+import com.javath.stock.settrade.Index;
 
-                        strbuf.append(Long.toString((int) buf[i] & 0xff, 16));
-                }
-
-                return strbuf.toString();
-        }
-
-        public static byte[] hexStringToByteArray(String s) {
-                int len = s.length();
-                byte[] data = new byte[len / 2];
-                for (int i = 0; i < len; i += 2) {
-                        data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character
-                                        .digit(s.charAt(i + 1), 16));
-                }
-                return data;
-        }
-
-        public static String stringToHex(String base) {
-                StringBuffer buffer = new StringBuffer();
-                int intValue;
-                for (int x = 0; x < base.length(); x++) {
-                        int cursor = 0;
-                        intValue = base.charAt(x);
-                        String binaryChar = new String(Integer.toBinaryString(base
-                                        .charAt(x)));
-                        for (int i = 0; i < binaryChar.length(); i++) {
-                                if (binaryChar.charAt(i) == '1') {
-                                        cursor += 1;
-                                }
-                        }
-                        if ((cursor % 2) > 0) {
-                                intValue += 128;
-                        }
-                        buffer.append(Integer.toHexString(intValue));
-                }
-                return buffer.toString();
-        }
-
-        public static void main(String[] args) throws Exception {
-
-                String plainText = null;
-                String key = null;
-                
-                if (args.length == 2) {
-                        plainText = args[0];
-                        key = args[1];
-                } else {
-                        System.err.println("params text key");
-                        System.exit(1);
-                }
-
-                if (key.length() != 16) {
-                        System.err.println("key length must be 16 characters.");
-                        System.exit(1);
-                }
-
-                System.out.println("Key: " + key);
-                System.out.println("Key (HEX): " + stringToHex(key));
-                
-                byte[] raw = hexStringToByteArray(stringToHex(key));
-
-                SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
-
-                // Instantiate the cipher
-                Cipher cipher = Cipher.getInstance("AES");
-
-                cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
-
-                System.out.println("Key (Byte to Hex): " +asHex(raw));
-
-                byte[] encrypted = cipher
-                                .doFinal((args.length == 0 ? "This is just an example"
-                                                : args[0]).getBytes());
-                System.out.println("encrypted string: " + asHex(encrypted));
-
-                cipher.init(Cipher.DECRYPT_MODE, skeySpec);
-                byte[] original = cipher.doFinal(encrypted);
-                String originalString = new String(original);
-                System.out.println("Original string: " + originalString);
-        }
-
+public class AES extends Object {
+	
+	private final Cipher cipher_encrypt;
+	private final Cipher cipher_decrypt;
+	
+	public AES(String key) {
+		this(Base64.decodeBase64(key));
+	}
+	
+	public AES(byte[] key) {
+		try {
+			SecretKeySpec skeySpec = new SecretKeySpec(key, "AES");
+			cipher_encrypt = Cipher.getInstance("AES");
+			cipher_encrypt.init(Cipher.ENCRYPT_MODE, skeySpec);
+			cipher_decrypt = Cipher.getInstance("AES");
+			cipher_decrypt.init(Cipher.DECRYPT_MODE, skeySpec);
+		} catch (NoSuchAlgorithmException e) {
+			logger.severe(message(e));
+			throw new ObjectException(e);
+		} catch (NoSuchPaddingException e) {
+			logger.severe(message(e));
+			throw new ObjectException(e);
+		} catch (InvalidKeyException e) {
+			logger.severe(message(e));
+			throw new ObjectException(e);
+		}
+	}
+	
+	public String encrypt(String message) {
+		byte[] encrypted;
+		try {
+			encrypted = cipher_encrypt
+			        .doFinal(message.getBytes("UTF-8"));
+		} catch (IllegalBlockSizeException e) {
+			logger.severe(message(e));
+			throw new ObjectException(e);
+		} catch (BadPaddingException e) {
+			logger.severe(message(e));
+			throw new ObjectException(e);
+		} catch (UnsupportedEncodingException e) {
+			logger.severe(message(e));
+			throw new ObjectException(e);
+		}
+		return Base64.encodeBase64String(encrypted);
+	}
+	
+	public String decrypt(String encrypted) {
+		byte[] decrypted;
+		try {
+			decrypted = cipher_decrypt
+			        .doFinal(Base64.decodeBase64(encrypted));
+		} catch (IllegalBlockSizeException e) {
+			logger.severe(message(e));
+			throw new ObjectException(e);
+		} catch (BadPaddingException e) {
+			logger.severe(message(e));
+			throw new ObjectException(e);
+		}
+		return StringUtils.newStringUtf8(decrypted);
+	}
+	
 }
